0dfd431c94afc59d49884758fd00620b44e1cade
Alex Pounds <alex+git@alexpounds.com>
2020-04-12 22:33:18 -0400

    Include substream (only all photos for now) in photo page.

--- lib/piccle/renderer.rb
+++ lib/piccle/renderer.rb
@@ -38,6 +38,7 @@ module Piccle
       template_vars = {
         photo: photo_data,
         selector: selector,
+        substreams: [@parser.substream_for(hash)],
         canonical: "photos/#{hash}.html" # TODO: Other paths live in piccle.rake. Why's this one here?
       }
       template_vars[:include_prefix] = include_prefix(selector) if selector.any?
