a7e62ec4482a42a94fbead9de4860a34d259e214
Alex Pounds <alex+git@alexpounds.com>
2020-04-12 21:28:17 -0400

    Remove yet more unused code.

--- lib/piccle/template_paths.rb
+++ lib/piccle/template_paths.rb
@@ -1,20 +1,4 @@
 class Piccle::TemplatePaths
-  def self.index_path
-    "/"
-  end
-
-  def self.browse_by_date_path(year=nil, month=nil, day=nil)
-    if year && month && day
-      "/by-date/#{year}/#{month}/#{day}.html"
-    elsif year && month
-      "/by-date/#{year}/#{month}.html"
-    elsif year
-      "/by-date/#{year}.html"
-    else
-      "/by-date"
-    end
-  end
-
   def self.thumbnail_path(photo)
     "/images/thumbnails/#{photo.md5}.#{photo.file_name}"
   end
